6061b8a83b5846fc0a26e8365e4f75f58aa0c85e
levy <15210903179@163.com>
2017-06-30 11:40:49 +0800

    but there is a bug :below  android 5.0 there is an exception in the wifimanage

--- app/src/main/java/com/shuxiangbaima/netfile/wifinearby/WifiInfoActivity.java
+++ app/src/main/java/com/shuxiangbaima/netfile/wifinearby/WifiInfoActivity.java
@@ -7,8 +7,7 @@ import android.net.wifi.WifiInfo;
 import android.net.wifi.WifiManager;
 import android.os.Bundle;
 import android.os.Handler;
-import android.support.annotation.Nullable;
-import android.util.Log;
+import android.os.Message;
 import android.widget.TextView;
 
 import com.shuxiangbaima.netfile.R;
@@ -22,34 +21,16 @@ import java.util.TimerTask;
  */
 
 public class WifiInfoActivity extends Activity {
+
     private Timer timer;
     private TimerTask timerTask;
 
-    private Handler mHandler = new Handler() {
+
+    private Handler mHandler = new Handler(){
         public void handleMessage(android.os.Message msg) {
             switch (msg.what) {
                 case 0:
-                    WifiManager wm = (WifiManager) getSystemService(Context.WIFI_SERVICE);
-
-                    WifiInfo info = wm.getConnectionInfo();
-                    int strength = info.getRssi();
-                    int speed = info.getLinkSpeed();
-                    String units = WifiInfo.LINK_SPEED_UNITS;
-                    String ssid = info.getSSID();
-
-                    Log.e("", "=========handleMessage: "+wm);
-                    List<ScanResult> results = wm.getScanResults();
-                    String otherwifi = "The existing network is: \n\n";
-
-                    for (ScanResult result : results) {
-                        otherwifi += result.SSID + ":" + result.level + "\n";
-                    }
-
-                    String text = "We are connecting to " + ssid + " at " + String.valueOf(speed) + "   " + String.valueOf(units) + ". Strength : " + strength;
-                    otherwifi += "\n\n";
-                    otherwifi += text;
-
-                    tv.setText(otherwifi);
+                    getWifi();
                     break;
                 default:
                     break;
@@ -60,13 +41,29 @@ public class WifiInfoActivity extends Activity {
     private WifiManager wm;
 
     @Override
-    protected void onCreate(@Nullable Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.acitivity_wifinearby);
 
-        tv = (TextView) findViewById(R.id.tv_wifi);
-
+        tv  = (TextView) findViewById(R.id.tv_wifi);
         wm = (WifiManager) getSystemService(Context.WIFI_SERVICE);
+
+        getWifi();
+
+        timerTask = new TimerTask() {
+            @Override
+            public void run() {
+                Message msg = new Message();
+                msg.what = 0;
+                mHandler.sendMessage(msg);
+            }
+        };
+        timer = new Timer();
+        timer.schedule(timerTask,0,2000);
+    }
+
+    private void getWifi() {
+
 
         WifiInfo info = wm.getConnectionInfo();
         int strength = info.getRssi();
@@ -75,27 +72,20 @@ public class WifiInfoActivity extends Activity {
         String ssid = info.getSSID();
 
         String otherwifi = "The existing network is: \n\n";
-        List<ScanResult> results = wm.getScanResults();
 
+        List<ScanResult> results = wm.getScanResults();//安卓5.0版本以下报空指针异常
+        int i=0;
         for (ScanResult result : results) {
-            otherwifi += result.SSID + ":" + result.level + "\n";
+            otherwifi += result.SSID  + ":" + result.level + "\n";
+            if (++i==10){
+                break;
+            }
         }
-
 
         String text = "We are connecting to " + ssid + " at " + String.valueOf(speed) + "   " + String.valueOf(units) + ". Strength : " + strength;
         otherwifi += "\n\n";
         otherwifi += text;
 
         tv.setText(otherwifi);
-//        timerTask = new TimerTask() {
-//            @Override
-//            public void run() {
-//                Message msg = new Message();
-//                msg.what = 0;
-//                mHandler.sendMessage(msg);
-//            }
-//        };
-//        timer = new Timer();
-//        timer.schedule(timerTask, 0, 2000);
     }
 }
